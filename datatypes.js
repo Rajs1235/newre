47faa0c02e8551821dd82ddff6dfa3d61483b131
Raj Srivastava <161757777+Rajs1235@users.noreply.github.com>
2025-01-31 08:25:53 +0000

    datatype

---
+++ datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all js code as new version
+/*const accountId=144553
+let accountEmail="raj@google.com"
+var accountPassword="12345"
+accountCity="jaipur"
+let accountstate
+//accountId=2 not allowed
+console.log(accountId);
+accountEmail="hc@hc.com"
+accountPassword="abcde"
+accountCity="bhopal"
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountstate])*/
+//alert(3+3) -> we are using nodejs not browser
+
+console.log(2 + 3);
+console.log("hitesh");
+let name = "raj"
+let age = 18
+let isloggedin = false
+
+//number=> 2 to power 53
+//bigint
+//string
+//boolean
+//null=> standalone value
+//undefined=> abhi value asign nhi hui h
+//symbol=> unique
+console.log(typeof undefined);//data type btaega
+
