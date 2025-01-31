57dc68c218197a81b935dbd4047c5f4a518e5b9b
Raj Srivastava <161757777+Rajs1235@users.noreply.github.com>
2025-01-31 08:56:40 +0000

    operation

---
+++ operations.js
@@ -0,0 +1,17 @@
+// let value=3
+// let negvalue= -value
+// //console.log(negvalue)//simple minus lg jaega
+// let str1="hello"
+// let str2=" raj"
+// console.log(str1+str2)
+// console.log(1+"2")//=>12
+// console.log("1"+2) //=>12
+// console.log(1+2+"2") //=>32
+// console.log("1"+2+2)//=>122
+let gamecount=23
+console.log(gamecount);
+gamecount++
+console.log(gamecount);
+console.log(gamecount);
+++gamecount
+console.log(gamecount);
