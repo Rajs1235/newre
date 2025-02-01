221923aba82f3e79fc261e0003e132f105309499
Raj Srivastava <161757777+Rajs1235@users.noreply.github.com>
2025-02-01 08:20:09 +0000

    maths

---
+++ basics/maths.js
@@ -0,0 +1,38 @@
+/*const score=400000000
+
+console.log(score);
+console.log(score.toLocaleString());//method to add commas to no so 
+// that it becomes easy to look &understand foreign stnd
+console.log(score.toLocaleString('en-IN'));//method to add commas to no so 
+// that it becomes easy to look &understand indian stnd
+/*
+const balance=new Number(100)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));//2 decimal places 
+//precision ka ek method h isme 
+//kitne no tk ka precision chahiye
+const oth=123.8966
+console.log(oth.toPrecision(4));//4 sig digits 123.9
+console.log(oth.toPrecision(3));//3 sig digits 124
+console.log(oth.toPrecision(6));//6 sig digits 123.897
+console.log(oth.toPrecision(7));//7 sig digits 123.8966
+console.log(oth.toPrecision(8));//8 sig digits 123.89660*/
+//number.maxvalue,number.minvalue
+                /////////////////////MATHS////////////////////////////////
+/*console.log(Math) ;//object h 
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));//4
+console.log(Math.round(4.6));//5 
+//similar to cpp ceil and floor are also used*/
+console.log(Math.random());//random value  between 0&1
+console.log(Math.random()*10);//isme issue h ki maay be value 1 s chhoti aa jae
+console.log(Math.random()*10+1);//to avoid above issue
+
+const max=20
+const min=10
+
+console.log(Math.random()*(max-min+1)+min);//max aur min k beech koi bhi number de dega y
+
+
+
