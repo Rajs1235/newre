bcc2ef09276b45defe79284c0bb0351d849dcc28
Raj Srivastava <161757777+Rajs1235@users.noreply.github.com>
2025-02-01 08:20:15 +0000

    str

---
+++ basics/string.js
@@ -0,0 +1,28 @@
+/*const name="hitesh"
+const count=21
+/*console.log(name+" "+count);//older way not recommended
+console.log(`hello myself ${name} and count is ${count}`)
+**///declaration
+/*console.log(name.__proto__);//=>{}
+console.log(name.length);
+console.log(name.toUpperCase());//=>HITESH
+console.log(name);
+const gname='hitesh-hc';
+console.log(gname.charAt(2));//t
+console.log(gname.indexOf('t'));
+const s=gname.substring(0,4);//kha s kha tk substr
+console.log(s);
+console.log(gname);
+
+const n=gname.slice(-8,4)//start s end -1 tk dega  aage cpunt=0 s peeche -1 s
+console.log(n);*/
+const newstr="   raj"+"  srivastava"
+console.log(newstr)
+
+console.log(newstr.trim())//trim is used to remove leading spaces and newline char
+
+const url="https://curly-doodle-wrv996gq54q52g96q.github.dev/"
+
+console.log(url.replace('wrv','lun'))//replace suubstrings
+console.log(url.includes('mood'))
+console.log(newstr.split(' '))//gives an array of all strings seperated by ' '
