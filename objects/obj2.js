aa6b0305efd8e71097fb4b31eb5473012bfacaf2
Raj Srivastava <161757777+Rajs1235@users.noreply.github.com>
2025-02-05 14:20:56 +0000

    objects and json intro

--- objects/obj2.js
+++ objects/obj2.js
@@ -1,3 +1,4 @@
+/*
 const tinderuser={}
 tinderuser.id="123abc"
 tinderuser.name="sammy"
@@ -33,3 +34,25 @@ console.log(Object.values(tinderuser));//print all values
 console.log(Object.entries(tinderuser));//print all entry as an array of array
 
 console.log(tinderuser.hasOwnProperty('loggedin'));
+*/
+const course={
+    coursename:"js in hindi",
+    price:"999",
+    courseinstructor:"hitesh"
+}
+//instead of writing course.courseinstructor
+
+const {courseinstructor: instructor}=course
+console.log(instructor);
+//const navbar=({company})=> desstructure kiya jata h
+
+//api==> json
+/*{
+"name":"raj",
+"coursename":"js in hindi",
+"price":"free"
+}*/
+
+//api m khelte time ek unnamed object milega
+//now us object m key value pairs honge aur dono hi string type k honge
+//array k form m bhi milte h loops s access kr ke chl skte h
