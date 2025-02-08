79ef4376d97ca9bed045af7c9ff48194ccb7f328
Raj Srivastava <161757777+Rajs1235@users.noreply.github.com>
2025-02-08 13:58:54 +0000

    conditional & switch

---
+++ logic/one.js
@@ -0,0 +1,60 @@
+//conditional
+//note: == to comparision h but === is comparison based on data type
+//as well as value i.e. 2=="2" true de dega pr 2==="2" true dega
+//console.log(2=="2")
+/*
+const temp=45
+if(temp<50){
+    console.log("less than 50");
+    }
+   else console.log(">50");
+    
+   const score=200
+   if(score>100){
+    const power="fly"
+    console.log(`user power: ${power}`);
+    
+   }
+  // console.log(`user power: ${power}`);//not defined for the scope
+  const bal=1000
+  if(bal<500){
+    console.log("less than 500");
+    }
+    if(bal<750){
+        console.log("less than 750");
+        }
+  if(bal<1000){
+            console.log("less than 1000");
+            }
+else{
+                console.log("less than 1200");
+            }
+const userloggedin=true
+const debitcard=true
+const loggedinfr=true
+const loginviagoogle=false;
+const loginviaemail=true;
+if(userloggedin && debitcard && loggedinfr){
+     console.log("ha theek h");
+}
+   if(loginviagoogle||loginviaemail){
+    console.log("user logged in");
+    
+   }
+*/
+// switch can be used same as cpp
+const month=3
+switch(month){
+    case 1:console.log("jan");
+    break;
+    case 2:console.log("feb");
+    break;
+    case 3:console.log("mar");
+    break;
+    case 4:console.log("apr");
+    break;
+    case 5:console.log("may");
+    break;
+    case 6:console.log("jun");
+    break;
+}
