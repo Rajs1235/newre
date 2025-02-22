86c50cf367c58dfab261cf3e573300f8107acb31
Raj Srivastava <161757777+Rajs1235@users.noreply.github.com>
2025-02-22 12:58:56 +0000

    lopp3

--- filtermp/loops3.js
+++ filtermp/loops3.js
@@ -1,14 +1,15 @@
-const mynums=[1,2,3,4,5,6,7,8,9,10]
+const mynums=[1,2,3]
 
-const newnums=mynums.map((num)=>{ return num+10})
-//=>agr braces lga k scope bn gya to return keyword use krna padega
-console.log(newnums);
-
-
+// const newnums=mynums.map((num)=>{ return num+10})
+// //=>agr braces lga k scope bn gya to return keyword use krna padega
 // console.log(newnums);
 
-//chaining
-const newnums2=newnums
-.map((num)=>num*10).map((num)=>num+1).filter((num)=>num>=150)
 
-console.log(newnums2);
+// // console.log(newnums);
+
+// //chaining
+// const newnums2=newnums
+// .map((num)=>num*10).map((num)=>num+1).filter((num)=>num>=150)
+
+// console.log(newnums2);
+
