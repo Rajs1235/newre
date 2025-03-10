854d2b0074ccb80d34e1e57a811f48a6cef73a13
Raj Srivastava <161757777+Rajs1235@users.noreply.github.com>
2025-03-10 12:51:36 +0000

    practice

---
+++ assyncjs/async.js
@@ -0,0 +1,12 @@
+//javascript=> synchronus & single threaded(slow)
+
+//execution context:execute one line at a time
+
+//blocking code(block the program flow=>read file synchronus) vs non blocking 
+// code(does not block execution=>read file async)
+
+//web api or NODE
+
+//task queue makes js fast =>adds to call stack
+
+//fetch api is added to task queue makes promise queue with high priority
